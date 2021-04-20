de07b3903e055acc32af725fce37c9d5b42972c8
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-04-19 21:11:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// 3.	3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.	
+
+
+ 
+// 
+
+let numeros1=parseInt(prompt("Ingrese los numeros:"));
+let numero2=parseInt(prompt("Ingrese los numeros:"));
+let suma= (parseInt(numero1))+(parseInt(numero2));
+alert (parseInt(suma));
+
+
+
+
+
+
+
+// let cantidadnumeros= new Array(1).fill('');
+// let numero= new Array(parseInt(prompt("Ingresar cantidad de numeros"))).fill();
+// for (let i = 0; i < numero.length; i++) {
+// numero[i] = prompt (`digite el numero ${i+1}`);
+// 
+// }
+// 
+// 

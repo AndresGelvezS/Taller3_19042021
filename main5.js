8d114ac22bc3216abb449f1355778776b2c53e51
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-04-20 23:48:16 -0500

    Add files via upload

---
+++ main5.js
@@ -0,0 +1,9 @@
+//5.	Realizar un programa que permita escribir los 100 primeros pares.
+
+let numerospares;
+
+for(numerospares= 0; numerospares<= 100 ; numerospares++){
+	if(numerospares%2==0){
+	alert(`numero ${numerospares} de los 100 primeros pares`);
+	}
+}
